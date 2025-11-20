c40a5d5036b8b63b14c56d2ba0d9fc3711094c7a
LimJDG <90924836+Grafrath@users.noreply.github.com>
2025-11-20 18:12:18 +0900

    Create Operator.java

---
+++ eclipse-workspace/ex_1120/src/ch01/Operator.java
@@ -0,0 +1,29 @@
+package ch01;
+
+public class Operator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 10;
+		int y = 10;
+		
+		//증가후 사용 전위연산
+		System.out.println(++y);
+		
+		//사용후 증가 후위연산
+		System.out.println(x++);
+		System.out.println(x);
+		
+		//관계(비교)연산자 동등 ( ==, !=), 또는 크기( <, <=, >, >=)를 평가해서 boolean 타입인 true/false를 산출한다.
+		
+		//!는 not의 의미를 가지고 있다. True를 False로 False를 True로 바꿔준다.
+		//!을 사용할때에만 잠깐 반영됨
+		
+		//문자열을 비교할 때는 동등(==, !=)연산자 대신 equals()와 !equals()를 사용한다.
+		
+		//논리연산자 논리곱(&&), 논리합(||), 배타적 논리합(^), 논리부정(!)연산을 수행한다.
+
+	}
+
+}
