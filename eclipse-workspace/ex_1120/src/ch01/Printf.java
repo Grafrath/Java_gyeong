87e02bdaeb389b28c1e5f5588366b9625374c593
LimJDG <90924836+Grafrath@users.noreply.github.com>
2025-11-20 18:12:19 +0900

    Create Printf.java

---
+++ eclipse-workspace/ex_1120/src/ch01/Printf.java
@@ -0,0 +1,26 @@
+package ch01;
+
+public class Printf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name = "최가";
+		String address = "부평구";
+		int age = 20;
+		double height = 184.8;
+		
+		System.out.printf("제 이름은 %s입니다.\n",name);
+		System.out.printf("제 나이는 %d살입니다.\n",age);
+		System.out.printf("제 주소는 %s입니다.\n",address);
+		System.out.printf("제 키는 %.1f cm입니다.\n",height);
+		
+		int x, y;
+		x = 10;
+		y = 5;
+		
+		System.out.printf("%d 와 %d의 합은 %d 이다. \n", x, y, x + y);
+
+	}
+
+}
