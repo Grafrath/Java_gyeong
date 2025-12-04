43fff8617eb7ce53c12b729f07c4729260ed6d5d
LimJDG <90924836+Grafrath@users.noreply.github.com>
2025-12-04 09:49:21 +0900

    1204

--- eclipse-workspace/demo/src/main/java/com/example/demo/DemoApplication.java
+++ eclipse-workspace/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -11,3 +12,7 @@ public class DemoApplication {
 	}
 
 }
+
+//@Configuration: 이 클래스가 설정파일임을 알려주는 용도의 어노테이션
+//@ComponentScan : 자동으로 컴포넌트 클래스를 검색하여 컴포넌트와 빈 클래스를 등록함
+//@EnableAutoConfiguration : 스프링의 다양한 설정이 자동으로 구성되고 완료됨
